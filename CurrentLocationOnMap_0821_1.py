449c3cca54d429cb5426334005385c2c21af64ad
cdshadow <153569166+cdshadow@users.noreply.github.com>
2024-08-21 20:57:50 +0900

    Update CurrentLocationOnMap_0821_1.py

--- CurrentLocationOnMap_0821_1.py
+++ CurrentLocationOnMap_0821_1.py
@@ -12,21 +12,19 @@ geolocator = Nominatim(user_agent="geoapiExercises")
 # 위치 입력 필드
 user_input = st.text_input("위치 입력", "대전")  # 기본 입력 값을 대전으로 설정
 
-# Geolocation API를 통해 사용자 위치 가져오기
-if st.button("내 위치 찾기"):
-    if user_input:
-        location = geolocator.geocode(user_input)  # 사용자가 입력한 위치로 설정
 
-        if location:
-            st.write(f"위도: {location.latitude}, 경도: {location.longitude}")
+# Folium 지도 생성 함수
+def create_map():
+    # Folium 지도 설정 (대전광역시 중심)
+    map_obj = folium.Map(
+        location=[36.3504, 127.3845],
+        zoom_start=12,  # 줌 레벨 조정
+    )
 
-            # 위치를 표시할 지도 생성
-            m = folium.Map(location=[location.latitude, location.longitude], zoom_start=15)
+# Streamlit 레이아웃 설정
+#st.title('대전광역시 지리 정보 시각화')
 
-            # 현위치에 마커 표시
-            folium.Marker([location.latitude, location.longitude], tooltip="내 위치").add_to(m)
-
-            # 지도를 스트림릿 앱에 표시
-            folium_static(m)
-        else:
-            st.error("위치를 찾을 수 없습니다. 다시 시도해 주세요.")
+# 지도 생성 및 출력
+#st.header('대전광역시 지도')
+map_display = create_map()
+st_folium(map_display, width=1200, height=700)
